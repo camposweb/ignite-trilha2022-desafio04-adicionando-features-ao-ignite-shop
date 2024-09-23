1478bb09bbdef375b5ea457f5e546aba62d23c51
Leandro Campos <camposweb@gmail.com>
2024-09-22 22:24:02 -0300

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    fontFamily: {
+      roboto: 'var(--font-roboto)',
+    },
+    colors: {
+      gray1: '#121214',
+      gray2: '#202024',
+      gray3: '#8D8D99',
+      gray4: '#C4C4CC',
+      gray5: '#E1E1E6',
+      white: '#FFFFFF',
+      principal: '#00875F',
+      light: '#00B37E',
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
