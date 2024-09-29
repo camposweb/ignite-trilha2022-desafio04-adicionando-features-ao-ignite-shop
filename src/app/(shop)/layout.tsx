d5b148d331943d23fd46054c1d24a286ebfbddbf
Leandro Campos <camposweb@gmail.com>
2024-09-29 18:26:24 -0300

    fix: Ajustes de código

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -7,7 +7,7 @@ interface ShopLayoutProps {
 
 export default function ShopLayout({ children }: ShopLayoutProps) {
   return (
-    <main className="mx-auto lg:mb-32 mb-5 w-full max-w-[1440px]">
+    <main className="mx-auto mb-5 w-full max-w-[1440px] lg:mb-32">
       <Header />
       {children}
     </main>
--- src/app/components/Cart/index.tsx
+++ src/app/components/Cart/index.tsx
@@ -9,17 +9,15 @@ export function Cart() {
         <Collapsible.Trigger asChild className="">
           <button className="items-center rounded-md bg-elements p-2">
             <Handbag className="h-6 w-6 text-icon" />
-            <div className="absolute flex justify-center border-background -right-3 border-2 overflow-visible top-[-8px] h-6 w-6 items-center rounded-full bg-principal">
-              <span className="text-sm font-bold text-white">
-                1
-              </span>
+            <div className="absolute -right-3 top-[-8px] flex h-6 w-6 items-center justify-center overflow-visible rounded-full border-2 border-background bg-principal">
+              <span className="text-sm font-bold text-white">1</span>
             </div>
           </button>
         </Collapsible.Trigger>
       </div>
       <Collapsible.Content
         forceMount
-        className="fixed right-0 top-0 z-20 flex h-screen w-screen transition duration-200 ease-in-out flex-col bg-elements data-[state=closed]:hidden lg:w-[480px] lg:translate-x-0"
+        className="fixed right-0 top-0 z-20 flex h-screen w-screen flex-col bg-elements transition duration-200 ease-in-out data-[state=closed]:hidden lg:w-[480px] lg:translate-x-0"
       >
         <Collapsible.Trigger asChild>
           <button className="flex justify-end pr-8 pt-8 data-[state=closed]:hidden">
@@ -30,7 +28,7 @@ export function Cart() {
           <h1 className="font-roboto text-xl font-bold text-title">
             Sacola de compras
           </h1>
-          <div className="mt-8 h-[400px] md:h-full lg:h-[250px] 2xl:h-[520px] w-full gap-6 flex flex-col overflow-auto">
+          <div className="mt-8 flex h-[400px] w-full flex-col gap-6 overflow-auto md:h-full lg:h-[250px] 2xl:h-[520px]">
             <ItemCart />
             <ItemCart />
             <ItemCart />
