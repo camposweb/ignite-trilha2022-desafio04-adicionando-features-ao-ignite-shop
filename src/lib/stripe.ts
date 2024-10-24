0d9a406172e6a4ec23a8fcd797fc979ef67d410a
Leandro Campos <camposweb@gmail.com>
2024-10-24 20:28:45 -0300

    fix: Corrigindo variável no arquivo stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,12 @@
 import { env } from '@/env'
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(`${env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`, {
-  apiVersion: '2024-06-20',
-  appInfo: {
-    name: 'Ignite Shop',
+export const stripe = new Stripe(
+  `${env.NEXT_PUBLIC_VERCEL_STRIPE_SECRET_KEY}`,
+  {
+    apiVersion: '2024-06-20',
+    appInfo: {
+      name: 'Ignite Shop',
+    },
   },
-})
+)
